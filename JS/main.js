1d6c772086777e60d2087078ccff1805bfa682ca
tesla <l21200582@pachuca.tecnm.mx>
2024-05-26 20:13:05 -0600

    |V9|Correcion error de boton de restart, se añadio un limite para victoria y ajuste en velocidades|

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 const gameOverButton = document.getElementById("game-over-button");
+const victoryButton = document.getElementById("victory-button");
 const phaseMessage = document.getElementById("phase-message");
 
 // Cargar las imágenes de fondo para cada fase
@@ -9,7 +10,8 @@ const backgroundImages = [
     'assets/backgrounds/galaxy-background.jpeg',
     'assets/backgrounds/sky-background.jpg',
     'assets/backgrounds/river-background.jpeg',
-    'assets/backgrounds/valley-background.jpeg'
+    'assets/backgrounds/valley-background.jpeg',
+    'assets/backgrounds/house-background.jpeg'
 ];
 let currentBackgroundIndex = 0;
 let backgroundImage = new Image();
@@ -140,7 +142,7 @@ function createCircle() {
         let randomRadius = Math.floor(Math.random() * 30 + 35);
         let randomX = Math.random() * (canvas.width - 2 * randomRadius) + randomRadius;
         let randomY = canvas.height + randomRadius;
-        let randomSpeed = Math.random() * 0.8 + 0.2;
+        let randomSpeed = Math.random() * 0.8 + 0.3;
         let randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
         let creationValid = true;
         for (let j = 0; j < circlesArray.length; j++) {
@@ -159,8 +161,8 @@ function createCircle() {
 
 function increaseCircleSpeed() {
     for (let i = 0; i < circlesArray.length; i++) {
-        circlesArray[i].speed += 0.5;
-        circlesArray[i].dy = -0.3 * circlesArray[i].speed;
+        circlesArray[i].speed += 0.6;
+        circlesArray[i].dy = -0.2 * circlesArray[i].speed;
     }
 }
 
@@ -185,6 +187,9 @@ canvas.addEventListener('click', (event) => {
                 if (level % 3 === 0) {
                     changePhase(level / 3);
                 }
+                if (level === 18) {
+                    victory();
+                }
             }
             createCircle();
             break;
@@ -207,8 +212,10 @@ function changePhase(phase) {
     const phaseMessages = [
         "Fase 1 - Invasión Planetaria. Prepárate para defender tu planeta.",
         "Fase 2 - Pelea en el Cielo. Enfrenta a los invasores para evitar que lleguen a tu ciudad.",
-        "Fase 3 - Valle Central. El ultimo bastion de defensa.",
-        "Fase 4 - Los cubos. Sera este el fin?."
+        "Fase 3 - Valle Central. El último bastión de defensa.",
+        "Fase 4 - Los cubos. ¿Será este el fin?",
+        "Fase 5 - El fin.",
+        "Fase 6 - Victoria."
     ];
 
     currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
@@ -220,31 +227,58 @@ function changePhase(phase) {
     }, 3000);
 }
 
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+let gameState = "playing"; // Variable para manejar el estado del juego
 
-    for (let i = 0; i < circlesArray.length; i++) {
-        circlesArray[i].update(ctx);
-        if (circlesArray[i].shouldBeRemoved) {
-            circlesArray.splice(i, 1);
-            i--;
+function victory() {
+    gameState = "victory";
+    backgroundMusic.pause();
+    canvas.style.cursor = "default";
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar la pantalla
+    ctx.fillStyle = 'black'; // Establecer color de fondo negro
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Dibujar fondo negro
+    victoryButton.style.display = "block";
+    victoryButton.addEventListener('click', () => {
+        location.reload();
+    });
+}
+
+function animate() {
+    if (gameState === "playing") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < circlesArray.length; i++) {
+            circlesArray[i].update(ctx);
+            if (circlesArray[i].shouldBeRemoved) {
+                circlesArray.splice(i, 1);
+                i--;
+            }
         }
+        if (circlesArray.length < 1 + level * 0.6) {
+            createCircle();
+        }
+        requestAnimationFrame(animate);
+    } else if (gameState === "victory") {
+        // Detener el juego en el estado de victoria
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        circlesArray = [];  // Vaciar el array de círculos
+        
     }
-    if (circlesArray.length < 1 + level * 1.2) {
-        createCircle();
-    }
-    requestAnimationFrame(animate);
 }
 
 function endGame() {
+    gameState = "gameOver";
     backgroundMusic.pause();
     canvas.style.cursor = "default";
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar la pantalla
+    ctx.fillStyle = 'black'; // Establecer color de fondo negro
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Dibujar fondo negro
     gameOverButton.style.display = "block";
     gameOverButton.addEventListener('click', () => {
         location.reload();
     });
 }
+
 canvas.style.cursor = "url(assets/mouse-icon.png), auto";
 
 animate();
